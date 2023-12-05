7bca160d578bc73efb24846604ff9c6ef3f9dfa0
OliverL <119365594+OliverLastik@users.noreply.github.com>
2023-12-05 10:16:55 +0100

    Nahodenie modelov do admina

--- flase_app/admin.py
+++ flase_app/admin.py
@@ -3,6 +3,12 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, Location, Building, Workplace
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Owner)
+admin.site.register(Supplier)
+admin.site.register(Cylinder)
+admin.site.register(Gas)
+admin.site.register(CylinderLife)
+admin.site.register(CylinderChange)
 
 
 @admin.register(Building)
@@ -18,3 +24,39 @@ class WorkplaceAdmin(admin.ModelAdmin):
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ["name", "workplace"]
+	
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+@admin.register(Workplace)
+class WorkplaceAdmin(admin.ModelAdmin):
+    list_display = ["name", "building"]
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ["name", "workplace", "person_responsible"]
+
+@admin.register(Cylinder)
+class CylinderAdmin(admin.ModelAdmin):
+    list_display = ["barcode", "owner"]
+
+@admin.register(Gas)
+class GasAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+@admin.register(CylinderLife)
+class CylinderLifeAdmin(admin.ModelAdmin):
+    list_display = ["cylinder", "volume", "supplier", "pressure", "location", "is_connected", "gas", "is_current", "start_date", "end_date"]
+
+@admin.register(CylinderChange)
+class CylinderChangeAdmin(admin.ModelAdmin):
+    list_display = ["life", "timestamp", "user", "pressure", "location", "is_connected"]
