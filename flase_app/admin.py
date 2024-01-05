d70798ec90675763e6a6cdd813b585172a53002c
jozef <jozef.spirka7@gmail.com>
2024-01-05 15:43:01 +0100

    added login with username

--- flase_app/admin.py
+++ flase_app/admin.py
@@ -9,33 +9,41 @@ admin.site.register(User, UserAdmin)
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ["name"]
 
+
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
     list_display = ["name"]
+
 
 @admin.register(Building)
 class BuildingAdmin(admin.ModelAdmin):
     list_display = ["name"]
 
+
 @admin.register(Workplace)
 class WorkplaceAdmin(admin.ModelAdmin):
     list_display = ["name", "building"]
+
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ["name", "workplace", "person_responsible"]
 
+
 @admin.register(Cylinder)
 class CylinderAdmin(admin.ModelAdmin):
     list_display = ["barcode", "owner"]
+
 
 @admin.register(Gas)
 class GasAdmin(admin.ModelAdmin):
     list_display = ["name"]
 
+
 @admin.register(CylinderLife)
 class CylinderLifeAdmin(admin.ModelAdmin):
     list_display = ["cylinder", "volume", "supplier", "pressure", "location", "is_connected", "gas", "is_current", "start_date", "end_date"]
+
 
 @admin.register(CylinderChange)
 class CylinderChangeAdmin(admin.ModelAdmin):
