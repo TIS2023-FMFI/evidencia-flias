a4747e50747e1cf5c95bf38598e1be4af4fc0bfd
Adam Zahradník <adam@zahradnik.xyz>
2024-01-30 21:09:41 +0100

    added form, views, urls and templates

--- flase_app/forms.py
+++ flase_app/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from flase_app.models import Owner, Supplier
-from flase_app.models import User
+from flase_app.models import Owner, User, CylinderLife, Cylinder, Supplier
 
 
 class OwnerForm(forms.ModelForm):
@@ -31,10 +30,57 @@ class UserForm(forms.ModelForm):
             "is_active": _("Is Active"),
         }
 
+
 class SupplierForm(forms.ModelForm):
     class Meta:
         model = Supplier
         fields = ["name"]
         labels = {
             "name": _("Name"),
+        }
+
+
+class CylinderLifeForm(forms.ModelForm):
+    
+    barcode = forms.CharField(max_length=64)
+    owner = forms.ModelChoiceField(queryset=Owner.objects.all())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        instance = kwargs.get('instance')
+        if instance:
+            self.fields['barcode'].initial = instance.cylinder.barcode
+            self.fields['owner'].initial = instance.cylinder.owner
+
+        self.fields['owner'].required = False
+
+    def save(self, commit = True):
+        cylinder, created = Cylinder.objects.get_or_create(barcode=self.cleaned_data["barcode"], defaults={"owner": self.cleaned_data["owner"]})
+        life = super().save(commit=False)
+        if (not created):
+            cylinder.owner = self.cleaned_data["owner"]
+            cylinder.save()
+        life.cylinder = cylinder    
+        life.save()
+
+        return life
+    
+    class Meta:
+        model = CylinderLife
+        fields = ["barcode", "owner", "volume", "supplier", "pressure", "location", "is_connected", "gas", "note", "is_current"]
+        widgets = {
+            'note': forms.Textarea(attrs={'cols': 40, 'rows': 5}),
+        }        
+        labels = {
+            "barcode": _("Barcode"),
+            "owner": _("Owner"),
+            "volume": _("Volume"),
+            "supplier": _("Supplier"),
+            "pressure": _("Pressure"),
+            "location": _("Location"),
+            "is_connected": _("Is_connected"),
+            "gas": _("Gas"),
+            "note": _("Note"),
+            "is_current": _("Is_current"),
         }
