0cea5af75a87e514e1d2297394ab4f23d69f1295
Neverdpis <71711548+Neverdpis@users.noreply.github.com>
2024-02-08 21:43:44 +0100

    password recovery email functionality

--- flase/settings.py
+++ flase/settings.py
@@ -132,3 +132,11 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "/admin/"
+
+# SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'u5716035@gmail.com'
+EMAIL_HOST_PASSWORD = 'sogl qnkk tzox bkti'
