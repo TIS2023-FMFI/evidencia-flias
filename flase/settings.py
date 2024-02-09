c638d072e8488df0f9dd940cb975600aa7a2a660
Neverdpis <71711548+Neverdpis@users.noreply.github.com>
2024-02-09 14:12:10 +0100

    Update settings.py

--- flase/settings.py
+++ flase/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'u5716035@gmail.com'
-EMAIL_HOST_PASSWORD = 'sogl qnkk tzox bkti'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
